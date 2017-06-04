b5531c15169e378c132310ffe7072969d4f01db7
zhedd <zamulim@gmail.com>
2017-06-04 22:15:10 +0300

    Added UDP communication and removed unused functions

--- Client.py
+++ Client.py
@@ -1,34 +1,18 @@
 #the modules required
-import socket, sys
-
-
-#Prints help
-def display_help():
-    print '\nhelp :('
-
-#Asks the required inputs from user
-#This function is used if hostname and port number are not provided as command line arguments.
-#Returns the hostname and the port number given by the user.
-def set_host_and_port():
-    
-    #ask the necessary information politely from user (use a loop!) :) 
-    #if user reluctant: sys.exit(0)
-    #if user confused: display_help()
-    #if user done: break 
-    #if user clumsy or idiotic: print what went wrong and ask again
-
-    #OPTIONAL: ask if the user wants to use default values = the right values for this project
-
-    return host, port
+import socket, struct, sys
 
 #Connects to the server using TCP protocol,
 #negotiates extra features and encryption keys
 #and receives the server's UDP port and an identity token
+#After that, calls talk_with_server()
 #
-#Parameters: address = a tuple containing the values of host and port
-#[more parameters will be added later]
-#Returns the server's UDP port, identity token and encryption key
-def start_connection(address):
+#Parameters: server_host = target server hostname/IP address
+#            server_tcpport = target server TCP port
+def start_connection(server_host, server_tcpport):
+
+    #create a tuple that contains host and port
+    address = (server_host, server_tcpport)
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(address)
     s.sendall("HELLO")
@@ -43,8 +27,10 @@ def start_connection(address):
     data = data.split(" ")
     #now data is contains something like ['HELLO', 'abcde' 12345']
     
-    #return the identity token and UDP port
-    return data[1], data[2]
+    #talk_with_server() requires identity token and UDP address
+    talk_with_server(data[1], server_host, int(data[2]))
+
+    return
 
 
 #This function is used for communication with the server.
@@ -53,16 +39,58 @@ def start_connection(address):
 #Next, a random number of messages and are received from the server.
 #And finally, a message which tells the exchange is finished, is received.
 #
-#Parameters: address = a tuple containing server IP address and UDP port
-#[parameters will be updated later]
-def talk_with_server(address):
+#Parameters: token = CID
+#            server_host = target server hostname/IP address
+#            server_udpport = target server UDP port
+def talk_with_server(token, server_host, server_udpport):
 
-    return
+    #target address for messaging
+    address = (server_host, server_udpport)
+	
+    #create an UDP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	
+    #the first UDP message
+    message_out = "Hello from " + token
+ 
+    #Structure of UDP message = CID     ACK  EOM  Data remaining  Content length Content
+    #              data types:  char[8] Bool Bool unsigned short  unsigned short char[64]
 
-    #communicate
-    #end communication
+    #create a packed UDP message with all the required information as shown above
+    data_out = struct.pack('>8s??HH64s', token, True, False, 0, len(message_out), message_out)
+    
+    s.sendto(data_out, address)
 
-    #no need to return anything, possibly
+    while True:
+
+        #receive a message from addr
+        data_in, addr = s.recvfrom(1024)
+        print 'Received a message from %s:' % str(addr)
+
+        #unpack the message
+        token, ack, eom, remaining, length, message_in = struct.unpack('>8s??HH64s', data_in)
+        print(message_in)
+
+        #if the last message (end of message = true), break the loop
+        if eom == True:
+            break
+        
+        #remove the null padding
+        message_in = message_in.strip('\x00')
+        
+        #create a list containing all the words from the received message
+        message_out = message_in.split(' ')
+        #reverse their order
+        message_out = reversed(message_out)
+        #and create a string from the list
+        message_out = ' '.join(message_out)
+
+        #show the target IP address and port + the message
+        print("Sending message to %s: \n%s" % ((socket.gethostbyname(server_host), server_udpport), message_out))
+
+        #pack the message and send it
+        data_out = struct.pack('>8s??HH64s', token, True, False, 0, len(message_out), message_out)
+        s.sendto(data_out, address)
     
 
 def main():
@@ -76,11 +104,7 @@ def main():
     except (IndexError, ValueError):
         sys.exit(USAGE) #if something fails, show USAGE and exit
 
-    #create a tuple that contains host and port
-    address = (server_host, server_tcpport)
-
-    #get identity token and UDP port from the server
-    token, server_udpport = start_connection(address)
+    start_connection(server_host, server_tcpport)
 
     print 'Everything worked as expected! Good bye!'
 
